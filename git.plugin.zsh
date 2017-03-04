92a2991476fb82e3d66cb85c582fe690823d71fb
Dennis <martymcfly@posteo.de>
2017-03-04 21:42:49 +0100

    combine git.lib of oh-my-zsh with prezto

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -6,6 +6,8 @@
 # Functions
 #
 
+source git.zsh
+
 # The name of the current branch
 # Back-compatibility wrapper for when this function was defined here in
 # the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
