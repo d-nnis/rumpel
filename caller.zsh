f8d292856b689ab9090867e2c3a6a376eff6803d
Dennis <martymcfly@posteo.de>
2017-03-20 00:09:09 +0100

    repair ffmpeg-script

--- caller.zsh
+++ caller.zsh
@@ -3,6 +3,8 @@
 abspath=$0:h
 
 ffmpeg_webm() {
-  for file in $*
-  $abspath/ffmpeg_webmmp3.pl $*
+  for file in $*; do
+    echo $file
+    $abspath/ffmpeg_webmmp3.pl $file
+  done
 }
--- ffmpeg_webmmp3.pl
+++ ffmpeg_webmmp3.pl
@@ -12,13 +12,27 @@ my $input = pop @ARGV;
 my $aq = 4;  # audio quality;
 
 $input =~ /(.+)\.\w+$/;
-my $output = $1;
+my $output;
+if ($1) {
+  $output = $1;
+} else {
+  # no dot in file name found
+  $output = $input;
+}
 $output .= ".mp3";
 
 say "Inside Perl";
 my $exec = "ffmpeg -i '$input' -acodec libmp3lame -aq $aq '$output'";
-say $exec;
 
-my $el = `$exec`;
+my $ec = `$exec`;
+say "errorcode:-$?-";
+
+if ($ec) {
+  say "There was an error:$ec";
+  say "not removing $input";
+} else {
+  say "remove $input";
+  unlink $input;
+}
 
 say "Done.";
